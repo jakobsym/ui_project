0a1f0a1a51f596fa289efb92e30874860b045d9b
jakobsym <jakob.symchych@my.utsa.edu>
2024-02-26 15:23:23 -0600

    added google sign(in/out) prototype

--- twitch_app/src/App.jsx
+++ twitch_app/src/App.jsx
@@ -1,36 +1,61 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import * as jwtd from 'jwt-decode';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [user, setUser] = useState({})
 
+  //TODO: add React Context to create a 'global state' for created user object
+  function handleCallbackResponse(response) {
+    console.log("encoded jwt id token: " + response.credential)
+
+    // access user data behind jwt
+    var userObj = jwtd.jwtDecode(response.credential)
+    console.log(userObj)
+    setUser(userObj)
+    document.getElementById("signinDiv").hidden = true
+  }
+
+  function handleSignOut(event) {
+    // TODO: Fix to use 'global state'
+    setUser({})
+    document.getElementById("signinDiv").hidden = false;    
+  }
+  
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "1090300539037-cdh1ug430oi6gsealeg7hp2vrrras9l3.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signinDiv"),
+      { theme: "outline", size: "large"}
+    ) 
+    // use `google.accounts.id.prompt()`, or a button for login
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
       {/* Tailwind example on <h1> */}
       <h1 className='text-4xl italic rotate-180'>Vite + React</h1>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {user &&
+        <div>
+          <img src={user.picture}></img>
+          <h3>{user.name}</h3>
+        </div>
+      }
+
+      <div id="signinDiv"></div>
+
+      {/* user logged in,*/}
+      { Object.keys(user).length != 0 &&
+        <button onClick={(e) => handleSignOut()}>Sign out</button>
+      }
     </>
   )
 }
